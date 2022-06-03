2cc8834fa34c51ec807e3c0e0e5fb61b31c27b57
samlam67812 <90791465+samlam67812@users.noreply.github.com>
2022-06-04 01:38:47 +0800

    Updates

---
+++ precache-manifest.a50d07cd81526af103d21b824868bfc8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "362bbfd4271aab6eb42ca52756477896",
+    "url": "/stripe-clone/index.html"
+  },
+  {
+    "revision": "8bf9347f9ab9a2afc20c",
+    "url": "/stripe-clone/static/css/main.c866a211.chunk.css"
+  },
+  {
+    "revision": "9d9ce4078b442a71cc60",
+    "url": "/stripe-clone/static/js/2.e1e77988.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stripe-clone/static/js/2.e1e77988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bf9347f9ab9a2afc20c",
+    "url": "/stripe-clone/static/js/main.c5bcd8ef.chunk.js"
+  },
+  {
+    "revision": "65b6352d4b579f4a736e",
+    "url": "/stripe-clone/static/js/runtime-main.ed73f593.js"
+  },
+  {
+    "revision": "882a211ad0bae36b9c11715c9291f36b",
+    "url": "/stripe-clone/static/media/hero.882a211a.svg"
+  },
+  {
+    "revision": "86d618b5be1508bcbbb31d6393f089f6",
+    "url": "/stripe-clone/static/media/logo.86d618b5.svg"
+  },
+  {
+    "revision": "e908706c41907a9611ba9c4d61a32f7c",
+    "url": "/stripe-clone/static/media/phone.e908706c.svg"
+  }
+]);
